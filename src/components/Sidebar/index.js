eb207c56a5ce1934343dc29aa47a020ce2842d08
Vaishakh Variar <vaishakhvariar@gmail.com>
2023-10-04 22:50:15 +0530

    Random Commit

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,16 +1,16 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSub from '../../assets/images/logo_sub.png';
+import LogoV from '../../assets/images/logo-v1.png';
+// import LogoSub from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faBlog, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBlogger, faGithub, faInstagram, faInstagramSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/' >
-            <img src={LogoS} alt='Logo for Website' />
-            <img className='sub-logo' src={LogoSub} alt='Logo Sub' />
+            <img src={LogoV} alt='Logo for Website' />
+            {/* <img className='sub-logo' src={LogoSub} alt='Logo Sub' /> */}
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to='/'>
@@ -30,8 +30,19 @@ const Sidebar = () => (
                 </a>
             </li>
             <li>
-                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/vaishakhvariar/'> 
-                    <FontAwesomeIcon icon={(faLinkedin)} color='#4d4d4e' />
+                <a target='_blank' rel='noreferrer' href='https://github.com/vaishakhvariar'> 
+                    <FontAwesomeIcon icon={(faGithub)} color='#4d4d4e' />
+                </a>
+            </li>
+           
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://vvariarwrites.wordpress.com/'> 
+                    <FontAwesomeIcon icon={(faBlogger)} color='#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' href=' https://www.instagram.com/vaishakh_variar/'> 
+                    <FontAwesomeIcon icon={(faInstagram)} color='#4d4d4e' />
                 </a>
             </li>
         </ul>
